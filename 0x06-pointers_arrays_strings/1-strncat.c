9f2c1fa1cd6af8a0b383cc44220314fbb40b01b0
Jeffa25 <jeffahian@gmail.com>
2023-03-31 16:06:47 -0700

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,17 @@
 #include "main.h"
+
 /**
- * reset_to_98-Resets the value of a variable back to 98.
- * @n:Variable to be reset.
- * Return:Void.
+ * swap_int-Swaps the values of 2 integers.
+ * @a:First argument to be swapped.
+ * @b:second argument to be swapped.
+ *
+ * Return:void.
  */
 
-void reset_to_98(int *n)
+void swap_int(int *a, int *b)
 {
-	*n = 98;
+	int p = *a;
+	*a = *b;
+	*b = p;
+
 }
